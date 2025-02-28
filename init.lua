58efb71e174c0fa6c70a9d4d7571f3134067c2b3
unknown <bavleydanial@gmail.com>
2025-02-28 19:23:38 +1100

    More configurations for neovide

--- init.lua
+++ init.lua
@@ -56,4 +56,5 @@ if vim.g.neovide then
     vim.g.neovide_cursor_animation_length = 0.13
     vim.g.neovide_refresh_rate = 170
     vim.g.neovide_refresh_rate_idle = 5
+    vim.g.neovide_cursor_trail_size = 0.4
 end
